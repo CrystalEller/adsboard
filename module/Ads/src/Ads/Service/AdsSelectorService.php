db0d2f9b01dffede0b4156f1035e23214bffde10
CrystalEller <maxromanov34@gmail.com>
2015-04-17 20:28:54 +0300

    add AdsSelectorService

--- module/Ads/src/Ads/Service/AdsSelectorService.php
+++ module/Ads/src/Ads/Service/AdsSelectorService.php
@@ -4,8 +4,6 @@ namespace Ads\Service;
 
 
 use Doctrine\ORM\EntityManager;
-use Zend\ServiceManager\ServiceLocatorAwareInterface;
-use Zend\ServiceManager\ServiceLocatorInterface;
 
 class AdsSelectorService
 {
@@ -17,28 +15,71 @@ class AdsSelectorService
         $this->em = $em;
     }
 
+    public function countAdsByCategories($ids = null)
+    {
+        if (!empty($ids)) {
+            $number = $this->em
+                ->createQuery("SELECT COUNT(e.id) FROM Application\Entity\Ads e
+                                                                    WHERE e.categoryid IN (:catsId)")
+                ->setParameters(array(
+                    'catsId' => $ids,
+                ))
+                ->getSingleScalarResult();
+        } else {
+            $number = $this->em
+                ->createQuery("SELECT COUNT(e.id) FROM Application\Entity\Ads e")
+                ->getSingleScalarResult();
+        }
+
+        return $number;
+    }
+
+    public function countAdsByAttributes($ids = null)
+    {
+        if (!empty($ids)) {
+            $number = $this->em
+                ->createQuery("Select COUNT(ads.id) from Application\Entity\Ads ads JOIN
+                                        Application\Entity\Categories cat WITH
+                                        ads.categoryid=cat.id JOIN
+                                        Application\Entity\CategoryAttributes attr WITH
+                                        attr.catid=cat.id
+                                        WHERE attr.id IN (:attributesId)")
+                ->setParameters(array(
+                    'attributesId' => $ids,
+                ))
+                ->getSingleScalarResult();
+        } else {
+            $number = $this->em
+                ->createQuery("Select COUNT(ads.id) from Application\Entity\Ads ads JOIN
+                                        Application\Entity\Categories cat WITH
+                                        ads.categoryid=cat.id JOIN
+                                        Application\Entity\CategoryAttributes attr WITH
+                                        attr.catid=cat.id")
+                ->getSingleScalarResult();
+        }
+
+        return $number;
+    }
+
     public function getAdsByCategories($page, array $categoriesId = null, $limit = 10)
     {
         $offset = ($page == 0) ? 0 : ($page - 1) * $limit;
 
-        if (!empty($categoriesId) && is_array($categoriesId)) {
-            $ads = $this->em()
+        if (!empty($categoriesId)) {
+            $ads = $this->em
                 ->createQuery("Select c from Application\Entity\Ads c
-                                          WHERE c.categoryid IN (:catsId) LIMIT :offset,:limit")
+                                          WHERE c.categoryid IN (:catsId) ORDER BY c.created DESC")
                 ->setParameters(array(
-                    'catsId' => implode(',', $categoriesId),
-                    'offset' => $offset,
-                    'limit' => $limit
+                    'catsId' => $categoriesId,
                 ))
+                ->setMaxResults($limit)
+                ->setFirstResult($offset)
                 ->getResult(\Doctrine\ORM\Query::HYDRATE_ARRAY);
         } else {
-            $ads = $this->em()
-                ->createQuery("Select c from Application\Entity\Ads c
-                                          WHERE c.categoryid LIMIT :offset,:limit")
-                ->setParameters(array(
-                    'offset' => $offset,
-                    'limit' => $limit
-                ))
+            $ads = $this->em
+                ->createQuery("Select c from Application\Entity\Ads c ORDER BY c.created DESC")
+                ->setMaxResults($limit)
+                ->setFirstResult($offset)
                 ->getResult(\Doctrine\ORM\Query::HYDRATE_ARRAY);
         }
         return $ads;
@@ -48,19 +89,18 @@ class AdsSelectorService
     {
         $offset = ($page == 0) ? 0 : ($page - 1) * $limit;
 
-
-        $ads = $this->em()
+        $ads = $this->em
             ->createQuery("Select ads from Application\Entity\Ads ads JOIN
                                         Application\Entity\Categories cat WITH
                                         ads.categoryid=cat.id JOIN
                                         Application\Entity\CategoryAttributes attr WITH
                                         attr.catid=cat.id
-                                        WHERE c.id IN (:attributesId) LIMIT :offset,:limit")
+                                        WHERE attr.id IN (:attributesId)")
             ->setParameters(array(
-                'attributesId' => implode(',', $attributesId),
-                'offset' => $offset,
-                'limit' => $limit
+                'attributesId' => $attributesId,
             ))
+            ->setMaxResults($limit)
+            ->setFirstResult($offset)
             ->getResult(\Doctrine\ORM\Query::HYDRATE_ARRAY);
 
         return $ads;
